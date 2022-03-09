7ff3a6c7339506e8fc6cf54373727b936e5e2837
odisseus <odisseus@users.noreply.github.com>
2022-03-09 07:34:58 +0100

    use scala and 3rdparty classes

--- scala/lib_example/Example.scala
+++ scala/lib_example/Example.scala
@@ -1,7 +1,10 @@
 package lib_example
 
 import some.TestMessage
+import io.netty.util.collection.ByteCollections
 
 object Example {
   val testMessage = TestMessage()
+  val x: Option[String] = None
+  val y = ByteCollections.emptyMap()
 }
